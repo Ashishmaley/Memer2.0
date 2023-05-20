e4133d132c49b0399e90b476e95bb4100829a47f
I'm ÀŚHÌŚH  MÀŁEY <90534593+Ashishmaley@users.noreply.github.com>
2023-05-20 12:13:33 +0530

    ads implementation

--- app/src/main/java/com/example/apimaster/MainActivity.kt
+++ app/src/main/java/com/example/apimaster/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.apimaster
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -10,41 +11,73 @@ import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.google.android.gms.ads.AdError
+import com.google.android.gms.ads.AdRequest.*
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.FullScreenContentCallback
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 import retrofit2.*
 import retrofit2.converter.gson.GsonConverterFactory
+
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var adView : AdView
+    private var mInterstitialAd: InterstitialAd? = null
     private lateinit var myAdapter: MyAdapter
     private lateinit var refreshButton: Button
     private lateinit var recyclerView: RecyclerView
     private lateinit var swipeRefreshLayout: SwipeRefreshLayout
     private lateinit var progressBar: ProgressBar
-    private lateinit var cardu:CardView
-    private lateinit var cardv:CardView
+    private lateinit var cardu: CardView
+    private lateinit var cardv: CardView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         fetchData()
-        refreshButton=findViewById(R.id.refreshButton)
+        intersAdd()
+        refreshButton = findViewById(R.id.refreshButton)
         swipeRefreshLayout = findViewById(R.id.swipeRefreshLayout)
         swipeRefreshLayout.setOnRefreshListener {
-            fetchData() // Call the method to fetch data again
+            showAdd() // Call the method to fetch data again
         }
         refreshButton.setOnClickListener {
-            fetchData()
+            showAdd()
         }
 
     }
 
+    private fun showAdd() {
+        if (mInterstitialAd!=null)
+        {
+            mInterstitialAd?.fullScreenContentCallback=object :FullScreenContentCallback(){
+                override fun onAdDismissedFullScreenContent() {
+                    super.onAdDismissedFullScreenContent()
+                    fetchData()
+                }
+
+                override fun onAdFailedToShowFullScreenContent(p0: AdError) {
+                    super.onAdFailedToShowFullScreenContent(p0)
+                    fetchData()
+                }
+
+            }
+            mInterstitialAd?.show(this)
+        }
+        else{
+           fetchData()
+        }
+    }
     private fun fetchData() {
-        cardu=findViewById(R.id.cardView)
-        cardv=findViewById(R.id.cardView2)
+        cardu = findViewById(R.id.cardView)
+        cardv = findViewById(R.id.cardView2)
         recyclerView = findViewById(R.id.recyclerView)
         progressBar = findViewById(R.id.progressBar)
         progressBar.visibility = View.VISIBLE
-        cardu.visibility=View.VISIBLE
-        cardv.visibility=View.VISIBLE
+        cardu.visibility = View.VISIBLE
+        cardv.visibility = View.VISIBLE
         val retrofitBuilder = Retrofit.Builder()
             .baseUrl("https://meme-api.com/gimme/")
             .addConverterFactory(GsonConverterFactory.create())
@@ -56,8 +89,8 @@ class MainActivity : AppCompatActivity() {
         retrofitData.enqueue(object : Callback<MemeData?> {
             override fun onResponse(call: Call<MemeData?>, response: Response<MemeData?>) {
                 progressBar.visibility = View.GONE
-                cardu.visibility=View.GONE
-                cardv.visibility=View.GONE
+                cardu.visibility = View.GONE
+                cardv.visibility = View.GONE
                 recyclerView.visibility = View.VISIBLE
 
                 val obj = response.body()
@@ -77,4 +110,27 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
+    private fun intersAdd() {
+
+        var adRequest = Builder().build()
+
+        InterstitialAd.load(
+            this,
+            "ca-app-pub-1500145166835548/5415527257",
+            adRequest,
+            object : InterstitialAdLoadCallback() {
+                override fun onAdFailedToLoad(adError: LoadAdError) {
+                    adError?.toString()?.let { Log.d(TAG, it) }
+                    mInterstitialAd = null
+                }
+
+                override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                    Log.d(TAG, "Ad was loaded.")
+                    mInterstitialAd = interstitialAd
+                }
+            })
+
+    }
 }
+
+
